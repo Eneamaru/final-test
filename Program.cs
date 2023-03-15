187c68f4515e857c1bc9d16c0f9f895cabb977c3
eneamaru <rsmoff@gmail.com>
2023-03-15 12:38:05 +0400

    Создание метода для формирования массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array = {"jome", "kome", "lon", "nani", "op"};
+
+string[] GetShortStrings(string[] strings)
+{
+    int shortStringsCount = 0;
+    for (int i = 0; i < strings.Length; i++)
+    {
+        if (strings[i].Length <= 3)
+        {
+            shortStringsCount++;
+        }
+    }
+    string[] shortStrings = new string[shortStringsCount];
+    int j = 0;
+    for (int i = 0; i < strings.Length; i++)
+    {
+        if (strings[i].Length <= 3)
+        {
+            shortStrings[j] = strings[i];
+            j++;
+        }
+    }
+    return shortStrings;
+}
